64d5007c2718828c6f278c2fa63ab01f861e0578
Krzysiek <kwrobel1989@gmail.com>
2015-01-31 18:47:51 +0100

    Running button game finished

--- MceAppSamples/src/main/java/com/zinno/mceconf/samples/RunningButtonGameActivity.java
+++ MceAppSamples/src/main/java/com/zinno/mceconf/samples/RunningButtonGameActivity.java
@@ -17,7 +17,7 @@ import butterknife.InjectView;
 /**
  * Created by krzysztofwrobel on 31/01/15.
  */
-public class RunningButtonGameActivity extends BleServiceBindingActivity {
+public class RunningButtonGameActivity extends BleServiceBindingActivity implements SeekBar.OnSeekBarChangeListener {
     public static final String TAG = RunningButtonGameActivity.class.getSimpleName();
 
     private TiSensor<?> buttonSensor;
@@ -38,6 +38,9 @@ public class RunningButtonGameActivity extends BleServiceBindingActivity {
     public SeekBar player2StatusSeekBar;
 
     private boolean sensorEnabled;
+    private TiKeysSensor.SimpleKeysStatus player1KeyStatus;
+    private TiKeysSensor.SimpleKeysStatus player2KeyStatus;
+    private boolean gameStarted = true;
 
 
     @Override
@@ -48,6 +51,11 @@ public class RunningButtonGameActivity extends BleServiceBindingActivity {
         buttonSensor = TiSensors.getSensor(TiKeysSensor.UUID_SERVICE);
 
         ButterKnife.inject(this);
+
+        player1StatusSeekBar.setClickable(false);
+        player1StatusSeekBar.setOnSeekBarChangeListener(this);
+        player2StatusSeekBar.setClickable(false);
+        player2StatusSeekBar.setOnSeekBarChangeListener(this);
     }
 
     @Override
@@ -68,37 +76,64 @@ public class RunningButtonGameActivity extends BleServiceBindingActivity {
     }
 
     @Override
-    public void onServiceDiscovered() {
+    public void onServiceDiscovered(String deviceAddress) {
         sensorEnabled = true;
         Log.d(TAG, "onServiceDiscovered");
 
-        for (String address : getDeviceAddresses()) {
-            getBleService().enableSensor(address, buttonSensor, true);
-        }
+        getBleService().enableSensor(deviceAddress, buttonSensor, true);
 
     }
 
     @Override
-    public void onDataAvailable(String serviceUuid, String characteristicUUid, String text, byte[] data) {
-        Log.d(TAG, String.format("ServiceUUID: %s, CharacteristicUUIS: %s", serviceUuid, characteristicUUid));
+    public void onDataAvailable(String deviceAddress, String serviceUuid, String characteristicUUid, String text, byte[] data) {
+        Log.d(TAG, String.format("DeviceAddress: %s,ServiceUUID: %s, CharacteristicUUIS: %s", deviceAddress, serviceUuid, characteristicUUid));
         Log.d(TAG, String.format("Data: %s", text));
 
         TiSensor<?> tiSensor = TiSensors.getSensor(serviceUuid);
         final TiKeysSensor tiKeysSensor = (TiKeysSensor) tiSensor;
         TiKeysSensor.SimpleKeysStatus simpleKeysStatus = tiKeysSensor.getData();
 
+        if (gameStarted && (simpleKeysStatus == TiKeysSensor.SimpleKeysStatus.ON_OFF || simpleKeysStatus == TiKeysSensor.SimpleKeysStatus.OFF_ON)) {
+
+            int playerNumber = getDeviceAddresses().indexOf(deviceAddress);
+            switch (playerNumber) {
+                case 0:
+                    if (player1KeyStatus != simpleKeysStatus) {
+                        player1StatusSeekBar.incrementProgressBy(1);
+                    }
+                    player1KeyStatus = simpleKeysStatus;
+                    break;
+                case 1:
+                    if (player2KeyStatus != simpleKeysStatus) {
+                        player2StatusSeekBar.incrementProgressBy(1);
+                    }
+                    player2KeyStatus = simpleKeysStatus;
+                    break;
+            }
+        }
 
     }
 
 
     @Override
-    public void onConnected() {
-        super.onConnected();
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (seekBar.getProgress() == seekBar.getMax()) {
+            gameStarted = false;
+            if (seekBar.equals(player1StatusSeekBar)) {
+                raceSummaryTextView.setText("Player 1 WON!");
+            } else if (seekBar.equals(player2StatusSeekBar)) {
+                raceSummaryTextView.setText("Player 2 WON!");
+            }
+        }
     }
 
     @Override
-    public void onDisconnected() {
-        super.onDisconnected();
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
     }
 
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
 }
