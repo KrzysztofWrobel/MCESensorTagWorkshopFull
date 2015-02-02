e4ec35341bfca89a6142df812654fe8c6d32495c
Krzysiek <kwrobel1989@gmail.com>
2015-02-02 21:22:15 +0100

    Sample reorder - the prettier the higher

--- MceAppSamples/src/main/java/com/zinno/mceconf/samples/Samples.java
+++ MceAppSamples/src/main/java/com/zinno/mceconf/samples/Samples.java
@@ -3,8 +3,8 @@ package com.zinno.mceconf.samples;
 public enum Samples {
     TWITTER(R.string.twitter_sample_name, R.string.twitter_sample_icon),
     BUTTON_GAME(R.string.button_game_sample_name, R.string.button_game_sample_icon),
-    ACCELEROMETER(R.string.accelerometer_sample_name, R.string.accelerometer_sample_icon),
     SAFE_LOCK(R.string.safe_lock_sample_name, R.string.safe_lock_sample_icon),
+    ACCELEROMETER(R.string.accelerometer_sample_name, R.string.accelerometer_sample_icon),
     TEMPERATURE(R.string.temperature_sample_name, R.string.temperature_sample_icon),
     MAGNETOMETER(R.string.magnetometer_sample_name, R.string.magnetometer_sample_icon),
     BAROMETER(R.string.barometer_sample_name, R.string.barometer_sample_icon),
