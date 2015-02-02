35e1e0fc61b7644dc0e4e0ff4cd7f872ba9c3b2b
Krzysiek <kwrobel1989@gmail.com>
2015-02-02 18:56:23 +0100

    Sample List Adapter changes

--- MceAppSamples/src/main/java/com/zinno/mceconf/samples/SamplesListAdapter.java
+++ MceAppSamples/src/main/java/com/zinno/mceconf/samples/SamplesListAdapter.java
@@ -13,6 +13,11 @@ import com.zinno.sensortag.BleServiceBindingActivity;
 import java.util.ArrayList;
 
 public class SamplesListAdapter extends RecyclerView.Adapter<SamplesListEntryViewHolder> {
+    //    public static final String MY_FIRST_SENSOR_TAG_MAC = "BC:6A:29:AC:7D:10"; // 34:B1:F7:D5:04:01
+    public static final String MY_FIRST_SENSOR_TAG_MAC = "BC:6A:29:AB:81:A9"; // 34:B1:F7:D5:04:01
+    public static final String MY_SECOND_SENSOR_TAG_MAC = "BC:6A:29:AB:45:79";
+
+
     Context context;
 
     public SamplesListAdapter(Context context) {
@@ -49,34 +54,31 @@ public class SamplesListAdapter extends RecyclerView.Adapter<SamplesListEntryVie
             case TWITTER:
                 intent = new Intent(this.context, TwitterActivity.class);
                 intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_NAME, "Button Sensor");
-//                intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESS, "34:B1:F7:D5:04:01");
-                intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESS, "BC:6A:29:AC:7D:10");
+                intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESS, MY_FIRST_SENSOR_TAG_MAC);
                 break;
             case HYGROMETER:
                 intent = new Intent(this.context, HygrometerActivity.class);
                 intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_NAME, "Hygrometer Sensor");
-//                intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESS, "34:B1:F7:D5:04:01");
-                intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESS, "BC:6A:29:AC:7D:10");
+                intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESS, MY_FIRST_SENSOR_TAG_MAC);
                 break;
             case TEMPERATURE:
                 intent = new Intent(this.context, TemperatureActivity.class);
                 intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_NAME, "Temperature Sensor");
-//                intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESS, "34:B1:F7:D5:04:01");
-                intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESS, "BC:6A:29:AC:7D:10");
+                intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESS, MY_FIRST_SENSOR_TAG_MAC);
                 break;
             case MAGNETOMETER:
                 intent = new Intent(this.context, MagnetometerActivity.class);
                 ArrayList<String> deviceNames = new ArrayList<>();
                 deviceNames.add("Sensor Tag");
                 ArrayList<String> deviceAddresses = new ArrayList<>();
-                deviceAddresses.add("BC:6A:29:AB:81:A9");
+                deviceAddresses.add(MY_FIRST_SENSOR_TAG_MAC);
                 intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_NAMES, deviceNames);
                 intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESSES, deviceAddresses);
                 break;
             case ACCELEROMETER:
                 intent = new Intent(this.context, DiceActivity.class);
                 intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_NAME, "Dice Sensor");
-                intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESS, "BC:6A:29:AB:81:A9");
+                intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESS, MY_FIRST_SENSOR_TAG_MAC);
                 break;
             case BUTTON_GAME:
                 intent = new Intent(this.context, RunningButtonGameActivity.class);
@@ -84,23 +86,22 @@ public class SamplesListAdapter extends RecyclerView.Adapter<SamplesListEntryVie
                 deviceNames1.add("Player 1 pad");
                 deviceNames1.add("Player 2 pad");
                 ArrayList<String> deviceAddresses1 = new ArrayList<>();
-                deviceAddresses1.add("BC:6A:29:AB:81:A9");
-                deviceAddresses1.add("BC:6A:29:AB:45:79");
+                deviceAddresses1.add(MY_FIRST_SENSOR_TAG_MAC);
+                deviceAddresses1.add(MY_SECOND_SENSOR_TAG_MAC);
                 intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_NAMES, deviceNames1);
                 intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESSES, deviceAddresses1);
                 break;
             case GYROSCOPE:
                 intent = new Intent(this.context, GyroscopeActivity.class);
                 intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_NAME, "SensorTag");
-//                intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESS, "34:B1:F7:D5:04:01");
-                intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESS, "BC:6A:29:AC:7D:10");
+                intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESS, MY_FIRST_SENSOR_TAG_MAC);
                 break;
             case BAROMETER:
                 intent = new Intent(this.context, PressureActivity.class);
                 ArrayList<String> deviceNames2 = new ArrayList<>();
                 deviceNames2.add("Sensor Tag");
                 ArrayList<String> deviceAddresses2 = new ArrayList<>();
-                deviceAddresses2.add("BC:6A:29:AB:45:79");
+                deviceAddresses2.add(MY_FIRST_SENSOR_TAG_MAC);
                 intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_NAMES, deviceNames2);
                 intent.putExtra(BleServiceBindingActivity.EXTRAS_DEVICE_ADDRESSES, deviceAddresses2);
                 break;
